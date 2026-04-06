2e54388d191c08c3b70847dc19e0b03dea5b5bfa
Riju-Das <riju.das1910@gmail.com>
2026-04-07 03:50:36 +0530

    configured docker-compose.yml for kafka and chromadb

---
+++ spring-backend/src/main/java/com/project/contract_audit/ContractAuditApplication.java
@@ -0,0 +1,13 @@
+package com.project.contract_audit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractAuditApplication.class, args);
+	}
+
+}
---
+++ spring-backend/src/test/java/com/project/contract_audit/ContractAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.contract_audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractAuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
