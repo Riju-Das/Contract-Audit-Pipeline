bc63c8ab930fb5acc948619f0af7f7e14aa6cd4e
Riju-Das <riju.das1910@gmail.com>
2026-05-15 01:56:26 +0530

    feat: Implemented Kafka Consumer Service on springboot and changed Violation Dto

--- spring-backend/src/main/java/com/project/contract_audit/dto/ViolationDto.java
+++ spring-backend/src/main/java/com/project/contract_audit/dto/ViolationDto.java
@@ -1,16 +1,38 @@
 package com.project.contract_audit.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @Builder
-public record ViolationDto (
-    @JsonProperty("chunk_index")  int chunkIndex,
-    @JsonProperty("chunk_text")  String chunkText,
-    @JsonProperty("legal_principle")  String legalPrinciple,
-    @JsonProperty("matched_policy")  String matchedPolicy,
-    @JsonProperty("confidence")  int confidence,
-    @JsonProperty("severity") String severity,
-    @JsonProperty("reasoning")  String reasoning,
-    @JsonProperty("source_file") String sourceFile
-){}
+@NoArgsConstructor
+@AllArgsConstructor
+public class ViolationDto {
+
+    @JsonProperty("chunk_index")
+    private int chunkIndex;
+
+    @JsonProperty("chunk_text")
+    private String chunkText;
+
+    @JsonProperty("legal_principle")
+    private String legalPrinciple;
+
+    @JsonProperty("matched_policy")
+    private String matchedPolicy;
+
+    @JsonProperty("confidence")
+    private int confidence;
+
+    @JsonProperty("severity")
+    private String severity;
+
+    @JsonProperty("reasoning")
+    private String reasoning;
+
+    @JsonProperty("source_file")
+    private String sourceFile;
+}
---
+++ spring-backend/src/main/java/com/project/contract_audit/kafka/KafkaResultConsumer.java
@@ -0,0 +1,78 @@
+package com.project.contract_audit.kafka;
+
+
+import com.project.contract_audit.model.ContractRecord;
+import com.project.contract_audit.model.Violation;
+import com.project.contract_audit.repository.ContractRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaResultConsumer {
+
+    private final ContractRepository contractRepository;
+
+    @KafkaListener(
+            topics = "contract.audit.result",
+            groupId = "spring-audit-result-consumer"
+    )
+    @Transactional
+    public void onAuditResult(
+            @Payload AuditResultEvent event,
+            @Header(KafkaHeaders.RECEIVED_PARTITION) int partition,
+            @Header(KafkaHeaders.OFFSET) long offset
+    ){
+
+        log.info("Received audit result: contractId={}, partition={}, offset={}",
+                event.getContractId(), partition, offset);
+
+        ContractRecord record = contractRepository.findById(event.getContractId())
+                .orElseGet(()->{
+
+                    log.warn("ContractRecord not found for id={}, creating new",
+                            event.getContractId());
+
+                    return ContractRecord.builder()
+                            .userId(event.getUserId())
+                            .filename(event.getFilename())
+                            .build();
+
+                });
+
+        List<Violation> violations = event.getViolations().stream()
+                .map(v -> Violation.builder()
+                        .chunkIndex(v.getChunkIndex())
+                        .chunkText(v.getChunkText())
+                        .severity(v.getSeverity())
+                        .legalPrinciple(v.getLegalPrinciple())
+                        .matchedPolicy(v.getMatchedPolicy())
+                        .confidence(v.getConfidence())
+                        .reasoning(v.getReasoning())
+                        .sourceFile(v.getSourceFile())
+                        .contractRecord(record)
+                        .build()
+                )
+                .toList();
+
+        record.setViolations(violations);
+        record.setTotalViolations(event.getTotalViolations());
+
+        contractRepository.save(record);
+
+        log.info("Saved audit result to DB: contractId={}, violations={}",
+                event.getContractId(), event.getTotalViolations());
+
+
+    }
+
+}
